16f4b8369f163883b72943ad43a19aecc4edce9e
roney ruiz <roneyruizrojas@gmail.com>
2024-11-09 22:25:50 -0600

    feat: first example with express 5.0

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import express, { type Request, type Response } from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/test", (req: Request, res: Response) => {
+  res.json({ message: "it is working babe! this is express 5.0" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening at http://localhost:${PORT}`);
+});
